c2dbccce75cc25403112ffa7f4dcef5a062b0c1a
Joshualgl <lg56578375@163.com>
2017-11-08 21:56:38 +0800

    第二天学习Java

---
+++ item/src/Number.java
@@ -0,0 +1,14 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+       byte myByte = 124;
+       short myShort = 32564;
+       int myInt = 45784612;
+       long myLong =  46789451;
+       long result = myByte + myShort + myInt + myLong;
+       System.out.println("result:"+result);
+	}
+
+}
---
+++ item/src/Number/First.java
@@ -0,0 +1,12 @@
+package Number;
+
+public class First {
+    static String s1 = "你好";
+	public static void main(String[] args){
+		// TODO 自动生成的方法存根
+        String s2 = "Java";
+        System.out.println(s1);
+        System.out.println(s2);
+	}
+}
+														
